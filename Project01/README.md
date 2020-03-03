35973667154edbd2de72c1cffe46ee166faf389d
Mridul Arora <aroram15@mcmaster.ca>
2020-03-03 02:44:18 +0000

    Submitting Project 01 Part 2

--- Project01/README.md
+++ Project01/README.md
@@ -28,12 +28,41 @@
                              by the user.
        4.2 **Execution**   : This feature can be executed by entering 3. as the first input when prompted to do so and                              
                              entering the extension of the file on the second attempt. 
-                             
-### 5. Custome Feature
-       5.1 First custom feature will incorporate security feature in project_analyze.sh file. If the user enters the correct
+
+### 5. Feature 04
+       5.1 **Description** : This feature will backup all the files with an extension of tmp and can also restore them as 
+                             specified by user during the prompt.
+       5.2 **Execution**   : This feature can be executed by entering 4. as the first input when prompted to do so and                              
+                             again asks user either for backup or restore during the next prompt.
+       5.3 **References**  : https://docstore.mik.ua/orelly/unix/upt/ch45_18.htm
+
+### 6. Feature 05
+       6.1 **Description** : This feature will do as per the assignment requirements.
+       6.2 **Execution**   : This feature can be executed by entering 5. as the first input when prompted to do so.
+
+### 7. Feature 06 ( Custom Feature )
+       7.1 **Description** : User will be allowed to enter the file or is allowed to execute the file after the user 
+                             enters the correct password for the file. User can also change the password.
+                             Remember not to delete password file.
+       7.2 **Execution**   : This feature can be executed by entering 6. as the first input when prompted to do so and
+                             can easily follow along.
+
+### 8. Feature 07 ( Custom Feature )
+       8.1 **Description** : This feature allows user to sort his files alphabatically.
+       8.2 **Execution**   : This feature can be executed by entering 7. as the first input when prompted to do so and
+                             can easily follow along.
+       8.3 **References**  : https://unix.stackexchange.com/questions/289228/sorting-files-using-a-bash-script
+                             This site helped my a lot to achieve the final output of the feature.
+
+### 9. Feature 08 ( Custom Feature )
+       9.1 **Description** : This feature will do the exact same thing as required by the project.
+       9.2 **Execution**   : This feature can be executed by entering 8. as the first input when prompted to do so and
+                             can easily follow along.                             
+###    Custome Feature
+       1.  First custom feature will incorporate security feature in project_analyze.sh file. If the user enters the correct
            password, only then he/she will be allowed to execute the file. Also the user will have an option to change the 
            password.
-       5.2 Second custom feature will sort the files in the repository according to the user input. For example if the user
+       2.  Second custom feature will sort the files in the repository according to the user input. For example if the user
            enters name then files will be arranged alphabetically.
        
 ###### ***Note*** All features will have statements giving user the instructions on the type of input the user can enter.
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,71 +1,251 @@
 #!/bin/bash
 
-echo "1. FIXME Log"
-echo "2. File Size List"
-echo "3. File Type Count"
-read -p "Enter the feature : " inp 
+read -p "Enter the password to execute further commands " pswd
+z=$(cat password)
+if [ $pswd == $z ] ; then
+	echo "1. FIXME Log"
+	echo "2. File Size List"
+	echo "3. File Type Count"
+	echo "4. Backup and Restore"
+	echo "5. Tag "
+	echo "6. Change Password"
+	echo "7. Arrange in alphabatical order"
+	echo "8. Checkout Latest Merge"
+	read -p "Enter the feature : " inp 
 
-if [ "$inp" -eq 1 ] ; then
-	s=$(find .. -type f)
-	if [ -f "tempfixme1" ] ; then
-		echo "inside 1"
-		echo "$s" > tempfixme1
-	else
-		touch tempfixme1
-		echo "inside 2"
-		echo "$s" > tempfixme1
-	fi
-	if [ -f "fixme.log" ] ; then
-		rm fixme.log
-	fi
-	noflines=($(wc -l tempfixme1))
-	for ((i=1 ; i<"$noflines"+1 ; i++)) ; do
-		ch=$(sed "${i}q;d" tempfixme1)
-		ta=$(tail -1 "$ch")
-		if [ "$ta" == *"#FIXME"* ] ; then
-			if [[ -f "fixme.log" ]] ; then
-				echo "$ch" >> fixme.log
-			else
-				touch fixme.log
-				echo "$ch" >> fixme.log
-			fi
+	if [ "$inp" -eq 1 ] ; then
+		s=$(find .. -type f)
+		if [ -f "tempfixme1" ] ; then
+			echo "inside 1"
+			echo "$s" > tempfixme1
+		else
+			touch tempfixme1
+			echo "inside 2"
+			echo "$s" > tempfixme1
 		fi
-	done
-	rm tempfixme1
-
-elif [ "$inp" -eq 2 ] ; then
-	s=$(find .. -type f)
-	if [ -f "tempfile1" ] ; then
-		echo "$s" > tempfile1
-	else
-		touch tempfile1
-		echo "$s" > tempfile1
-	fi
-	noflines=($(wc -l tempfile1))
-	if [ -f "tempfile2" ] ; then
-		truncate -s 0 tempfile2
-	fi
-	if [ -f "tempfile3" ] ; then
-		truncate -s 0 tempfile3
-	fi
-	for ((i=1 ; i<"$noflines"+1 ; i++)) ; do
-		ch=$(sed "${i}q;d" tempfile1)
-		size=($(stat --printf="%s" "$ch"))
-			if [ -f "tempfile2" ] ; then
-				echo "$size     :     $ch" >> tempfile2
-			else
-				touch tempfile2
-				echo "$size     :     $ch" >> tempfile2
+		if [ -f "fixme.log" ] ; then
+			rm fixme.log
+		fi
+		noflines=($(wc -l tempfixme1))
+		for ((i=1 ; i<"$noflines"+1 ; i++)) ; do
+			ch=$(sed "${i}q;d" tempfixme1)
+			ta=$(tail -1 "$ch")
+			if [ "$ta" == *"#FIXME"* ] ; then
+				if [[ -f "fixme.log" ]] ; then
+					echo "$ch" >> fixme.log
+				else
+					touch fixme.log
+					echo "$ch" >> fixme.log
+				fi
 			fi
-	done
-	ans=$(sort -n tempfile2)
-	rm tempfile*
-	echo "$ans" 
+		done
+		rm tempfixme1
 
-elif [ "$inp" -eq 3 ] ; then 
-	read -p "Enter the file extension:  " us
-	ans=$(find .. -type f -name "*.$us" | wc -l)
-	echo "$ans"
-else 
-	echo "Wrong Input"
+	elif [ "$inp" -eq 2 ] ; then
+		s=$(find .. -type f)
+		if [ -f "tempfile1" ] ; then
+			echo "$s" > tempfile1
+		else
+			touch tempfile1
+			echo "$s" > tempfile1
+		fi
+		noflines=($(wc -l tempfile1))
+		if [ -f "tempfile2" ] ; then
+			truncate -s 0 tempfile2
+		fi
+		if [ -f "tempfile3" ] ; then
+			truncate -s 0 tempfile3
+		fi
+		for ((i=1 ; i<"$noflines"+1 ; i++)) ; do
+			ch=$(sed "${i}q;d" tempfile1)
+			size=($(stat --printf="%s" "$ch"))
+				if [ -f "tempfile2" ] ; then
+					echo "$size     :     $ch" >> tempfile2
+				else
+					touch tempfile2
+					echo "$size     :     $ch" >> tempfile2
+				fi
+		done
+		ans=$(sort -n tempfile2)
+		rm tempfile*
+		echo "$ans" 
+
+	elif [ "$inp" -eq 3 ] ; then 
+		read -p "Enter the file extension:  " us
+		ans=$(find .. -type f -name "*.$us" | wc -l)
+		echo "$ans"
+
+	elif [ "$inp" -eq 4 ] ; then
+		echo "1. Backup"
+		echo "2. Restore"
+		read -p "Enter backup or restore according the given number i.e. 1 for Backup and 2 for Restore" inp
+		f=$(find .. -type f -name "*.tmp")
+		touch temp
+		echo "$f" > temp
+		if [ $inp -eq 1 ] ; then
+			if [ ! -d "backup" ] ; then
+				mkdir backup
+			elif [ -d "backup" ] ; then
+				rm -r backup
+				mkdir backup
+			fi
+			for x in $f ; do
+				mv $x ./backup
+			done
+			touch ./backup/restore.log
+			cp temp ./backup/restore.log
+		elif [ $inp -eq 2 ] ; then
+			touch filen
+			touch dirn
+			cat .//backup/restore.log | while read line
+			do
+				echo $(dirname "${line}") >> dirn
+				echo $(basename "${line}") >> filen
+			done
+			noflines=($(wc -l filen))
+			for ((i=1 ; i<"$noflines"+1 ; i++)) ; do
+				fl=$(sed "${i}q;d" filen)
+				dl=$(sed "${i}q;d" dirn)
+				mv ./backup/$fl $dl
+			done
+			rm dirn filen
+		fi
+		rm temp
+
+	elif [ "$inp" -eq 5 ] ; then
+		cd ..
+		read -p "Enter a tag for file " t
+		if [ -f ./Project01/"$t.log" ] ; then
+			rm ./Project01/"$t.log"
+		fi
+		touch ./Project01/"$t.log"
+		z=`find . -name "*.py" | xargs grep -h "#"* | grep -h "$t"`
+		echo "$z" > ./Project01/"$t.log"
+ 
+	elif [ "$inp" -eq 6 ] ; then
+		read -p "Enter the old password " opswd
+		if [ $opswd == $z ] ; then
+			read -p "Enter new password " npswd
+			echo $npswd > password
+			echo "Password changed successfully"
+		else
+			echo "Wrong old password"
+			echo "Restart the program to change the password"
+		fi
+
+	elif [ "$inp" -eq 7 ] ; then
+		function sort_f1 {
+ 			local ar
+  			mapfile -t ar < "$1"
+  			local x y t
+  			for ((x=0; x <= ${#ar[@]} - 2; x++))
+  			do
+    				for ((y=i + 1; y <= ${#ar[@]} - 1; y++))
+    				do
+      					local xval yval
+      					[[ ${ar[x]} =~ ([^[:space:]]+)[[:space:]]+(.*) ]]
+     					xval="${BASH_REMATCH[1]}"
+      					[[ ${ar[y]} =~ ([^[:space:]]+)[[:space:]]+(.*) ]]
+      					yval=${BASH_REMATCH[1]}
+      					if [[ $xval > $yval ]]
+      					then
+        					t=${ar[x]}
+        					ar[x]=${ar[y]}
+        					ar[y]=$t
+      					fi
+    				done
+  			done
+			printf "%s\n" "${ar[@]}"
+			}
+
+		function sort_f2 {
+  			local ar
+  			mapfile -t ar < "$1"
+  			local x y t
+  			for ((x=0; x <= ${#ar[@]} - 2; x++))
+  			do
+    				for ((y=x + 1; y <= ${#ar[@]} - 1; y++))
+    				do
+      					local xval yval
+     					[[ ${ar[x]} =~ ([^[:space:]]+)[[:space:]]+(.*) ]]
+      					xval="${BASH_REMATCH[2]}"
+      					[[ ${ar[y]} =~ ([^[:space:]]+)[[:space:]]+(.*) ]]
+      					yval=${BASH_REMATCH[2]}
+      					if [[ $xval > $yval ]]
+      					then
+        					t=${ar[x]}
+        					ar[x]=${ar[y]}
+        					ar[y]=$t
+      					fi
+    				done
+  			done
+  			printf "%s\n" "${ar[@]}"
+			}
+
+		function sort_key_f1 {
+  			local a b k val
+  			while IFS=' ' read -r a b
+  			do
+    				k+=("$a")
+    				val+=("$a $b")
+  			done < "$1"
+
+  			local m n tk tv
+  			for ((m=0; m <= ${#k[@]} - 2; m++))
+ 			do
+    				for ((n=m + 1; n <= ${#k[@]} - 1; n++))
+    				do
+      					if [[ ${k[m]} > ${k[n]} ]]
+      					then
+        					# swap keys
+        					tk=${k[m]}
+        					k[m]=${k[n]}
+        					k[n]=$tk
+        					# swap values
+        					tv=${val[m]}
+       		 				val[m]=${val[n]}
+        					val[n]=$tv
+      					fi
+    				done
+  			done
+  			printf "%s\n" "${val[@]}"
+			}
+
+		function sort_key_f2 {
+  			local a b k val
+  			while IFS=' ' read -r a b
+  			do
+    				k+=("$b")
+    				val+=("$a $b")
+  			done < "$1"
+
+  			local m n tk tv
+  			for ((m=0; m <= ${#k[@]} - 2; m++))
+  			do
+    				for ((n=m + 1; n <= ${#k[@]} - 1; n++))
+    				do
+      					if [[ ${k[m]} -gt ${k[n]} ]]
+      					then
+      						# swap keys
+        					tk=${k[m]}
+        					k[m]=${k[n]}
+        					k[n]=$tk
+        					# swap values
+        					tv=${val[m]}
+        					val[m]=${val[n]}
+        					val[n]=$tv
+      					fi
+    				done
+  			done
+  			printf "%s\n" "${val[@]}"
+			}
+
+	elif [ "$inp" -eq 8 ] ; then
+		git log --all --grep='merge' -n 1 | grep -e ".{50}" | git checkout
+
+	else 
+		echo "Wrong Input"
+	fi
+else
+	echo "Wrong Password! Please try again by running the program again."
 fi
